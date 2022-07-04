4bca61597914fc2cb2b6e092189443a143b96d7d
tangjiahui <tangjiahui@jxhsja.com>
2022-07-04 12:19:49 +0800

    update

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,3 +1,5 @@
+import VueRouter from 'vue-router'
+
 const routes = [
   {
     path: '/',
@@ -6,5 +8,18 @@ const routes = [
   },
 ]
 
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject)
+    return originalPush.call(this, location, onResolve, onReject);
 
+  return originalPush.call(this, location).catch((err) => err);
+};
+
+VueRouter.prototype.replace = function replace(location, onResolve, onReject) {
+  if (onResolve || onReject)
+    return originalPush.call(this, location, onResolve, onReject);
+
+  return originalPush.call(this, location).catch((err) => err);
+};
 export default routes
